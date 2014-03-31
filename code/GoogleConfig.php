4763d537e6f299a93b74aca35ae9465d31293a39
Loz Calver <lozcalver@bigfork.co.uk>
2014-03-31 10:03:25 +0100

    Explicitly set $db visibility - future-proof for SilverStripe 3.2

--- code/GoogleConfig.php
+++ code/GoogleConfig.php
@@ -1,22 +1,25 @@
 <?php
 
-/**
- * @package googleanalytics
- */
 class GoogleConfig extends DataExtension {
 
-	static $db = array(
+	private static $db = array(
 		'GoogleAnalyticsTrackingID' => 'Varchar',
 		'GoogleAnalyticsParameters' => 'Text'
 	);
 
+	/**
+	 * @param FieldList $fields
+	 */
 	public function updateCMSFields(FieldList $fields) {
-		$fields->addFieldToTab("Root", new Tab('GoogleAnalytics'));
-		$fields->addFieldToTab("Root.GoogleAnalytics", $trackingID = new TextField("GoogleAnalyticsTrackingID", "Google Analytics Tracking ID"));
-		$fields->addFieldToTab("Root.GoogleAnalytics", $parameters = new TextareaField("GoogleAnalyticsParameters", "Additional Parameters"));
-	
-		$trackingID->setRightTitle("e.g. UA-XXXXXX-X");
-		$parameters->setRightTitle("<strong>Advanced users only.</strong> If you do not know what this field does, please leave it blank.");
+		$analyticsFields = FieldList::create(
+			TextField::create("GoogleAnalyticsTrackingID", "Google Analytics Tracking ID")
+				->setRightTitle("e.g. UA-XXXXXX-X"),
+			TextareaField::create("GoogleAnalyticsParameters", "Additional Parameters")
+				->setRightTitle("<strong>Advanced users only.</strong> 
+					If you do not know what this field does, please leave it blank.")
+		);
+
+		$fields->addFieldToTab("Root", Tab::create('GoogleAnalytics')->setChildren($analyticsFields));
 	}
 
 }
