a0010cd443173086f5ae8c1a17b9f73c24330d91
helpfulrobot <chris@silverstripe.com>
2016-01-01 06:05:17 +1300

    Converted to PSR-2

--- code/GoogleConfig.php
+++ code/GoogleConfig.php
@@ -1,30 +1,31 @@
 <?php
 
-class GoogleConfig extends DataExtension {
+class GoogleConfig extends DataExtension
+{
 
-	private static $db = array(
-		'GoogleAnalyticsTrackingID' => 'Varchar',
-		'GoogleAnalyticsParameters' => 'Text',
-		'GoogleAnalyticsConstructorParameters' => 'Text'
-	);
+    private static $db = array(
+        'GoogleAnalyticsTrackingID' => 'Varchar',
+        'GoogleAnalyticsParameters' => 'Text',
+        'GoogleAnalyticsConstructorParameters' => 'Text'
+    );
 
-	/**
-	 * @param FieldList $fields
-	 */
-	public function updateCMSFields(FieldList $fields) {
-		$analyticsFields = FieldList::create(
-			TextField::create("GoogleAnalyticsTrackingID", "Google Analytics Tracking ID")
-				->setRightTitle("e.g. UA-XXXXXX-X"),
-			TextareaField::create("GoogleAnalyticsParameters", "Additional Parameters")
-				->setRightTitle("<strong>Advanced users only.</strong> 
+    /**
+     * @param FieldList $fields
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $analyticsFields = FieldList::create(
+            TextField::create("GoogleAnalyticsTrackingID", "Google Analytics Tracking ID")
+                ->setRightTitle("e.g. UA-XXXXXX-X"),
+            TextareaField::create("GoogleAnalyticsParameters", "Additional Parameters")
+                ->setRightTitle("<strong>Advanced users only.</strong> 
 					If you do not know what this field does, please leave it blank."),
-			TextareaField::create("GoogleAnalyticsConstructorParameters", "Constructor Parameters")
-				->setRightTitle("<strong>Advanced users only.</strong> 
+            TextareaField::create("GoogleAnalyticsConstructorParameters", "Constructor Parameters")
+                ->setRightTitle("<strong>Advanced users only.</strong> 
 					If you do not know what this field does, please leave it blank. An object to be
 					passed as an argument to ga.create()")
-		);
+        );
 
-		$fields->addFieldToTab("Root", Tab::create('GoogleAnalytics')->setChildren($analyticsFields));
-	}
-
+        $fields->addFieldToTab("Root", Tab::create('GoogleAnalytics')->setChildren($analyticsFields));
+    }
 }
